bec14f218aa2e1d2894fa386a1b0a3640c02eb41
luoxiang723 <515088980@qq.com>
2020-11-14 09:01:32 +0800

    s

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ App({
     // baseImgUrl:'http://erp.hnhlgyl.net/',
     // partnerCode: '00',
     // partnerCode: '05', 
-    // partnerCode: '1000', 
-    // baseImgUrl: 'http://mmj.zksr.cn:8888/',
+    partnerCode: '1000', 
+    baseImgUrl: 'http://mmj.zksr.cn:8888/',
     // 15576615400 
     // partnerCode: '1005',
     // baseImgUrl: 'http://erp.djlscs.cn/',
@@ -39,7 +39,7 @@ App({
     // partnerCode: '1058',
 
     //  partnerCode: '1056', 
-     partnerCode: '1057',
+    //  partnerCode: '1057',
 
     // partnerCode: '1050',
     // baseImgUrl:'http://erp.wgjnh.com',
